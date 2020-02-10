da8a5a6512e35c077895a685102397c34a150bc7
PawelGajo <pudzian12312@gmail.com>
2020-02-10 23:18:11 +0100

    add infinity scroll module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MemeViewComponent } from './components/meme-view/meme-view.component';
 import { MemesApiService } from './services/memes-api.service';
 import { HttpClientModule } from '@angular/common/http';
-
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 
 @NgModule({
   declarations: [MemeViewComponent],
   imports: [
+    InfiniteScrollModule,
     CommonModule,
     HttpClientModule
   ],
