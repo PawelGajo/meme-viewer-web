a382485a0c83a66a9ed7c48b3d86259ec668327a
PawelGajo <pudzian12312@gmail.com>
2020-02-10 23:18:56 +0100

    auto fetch pages

--- src/app/core/components/meme-view/meme-view.component.ts
+++ src/app/core/components/meme-view/meme-view.component.ts
@@ -8,13 +8,23 @@ import { Observable } from 'rxjs';
   styleUrls: ['./meme-view.component.css']
 })
 export class MemeViewComponent implements OnInit {
-  memes: Observable<any[]>;
+  memes: any = [];
+  pageNumber = 1;
+  fetchNewPage = true;
   constructor(private memeApi: MemesApiService) { }
-  getImages(page) {
-    this.memeApi.getImages(page).subscribe( x => this.memes = x);
+  getImages(): void {
+    this.memeApi.getImages(this.pageNumber).subscribe( x => {
+      this.memes = [ ...this.memes, ...x.content];
+      this.fetchNewPage = true;
+    });
+  }
+  onScroll(): void {
+    if ( !this.fetchNewPage) return; // preventing fetch new page if already fetching.
+    this.pageNumber++;
+    this.getImages();
   }
   ngOnInit(): void {
-    this.getImages(1);
+    this.getImages();
   }
 
 }
